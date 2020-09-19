f5fde01e3c03e0da2c49c4e344399c38da1b4388
MartinsMessias <messiasads2017@gmail.com>
2020-09-18 19:31:15 -0500

    Configurado models.py com 'Author' e 'News'

--- api/models.py
+++ api/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=120, blank=False, null=False)
+
+    def __str__(self):
+        return self.name
+
+class Noticia(models.Model):
+    title = models.CharField(max_length=200, blank=False, null=False)
+    content = models.TextField(blank=False, null=False)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
