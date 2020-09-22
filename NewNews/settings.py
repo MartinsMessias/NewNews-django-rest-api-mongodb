535ffc7282d6696804cbe3d01056c08113af996b
MartinsMessias <messiasads2017@gmail.com>
2020-09-22 00:00:22 -0500

    Deploy no heroku

--- NewNews/settings.py
+++ NewNews/settings.py
@@ -31,9 +31,8 @@ STATICFILES_DIRS = (
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env.str('SECRET_KEY')
-
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
