1c9ac5db41a35979dcb428eb0ef967b14fe49354
MartinsMessias <messiasads2017@gmail.com>
2020-09-21 21:48:35 -0500

    Deploy no heroku

--- NewNews/settings.py
+++ NewNews/settings.py
@@ -158,7 +158,7 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
 }
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+#STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 try:
