aa18a71a67ad67dcd6ecc0ae281bb035ef861286
MartinsMessias <messiasads2017@gmail.com>
2020-09-22 15:56:13 -0500

    Deploy no heroku DEBUG_FALSE

--- NewNews/settings.py
+++ NewNews/settings.py
@@ -32,7 +32,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env.str('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
