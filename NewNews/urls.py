72050142e911657324f16943096e400e5193a533
MartinsMessias <messiasads2017@gmail.com>
2020-09-22 16:49:01 -0500

    Final

--- NewNews/urls.py
+++
@@ -1,9 +0,0 @@
-"""NewNews URL Configuration
-"""
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('', include('api.urls')),
-    path('admin/', admin.site.urls),
-]
--- api/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from api.models import News, Author
-
-admin.site.register(Author)
-admin.site.register(News)
